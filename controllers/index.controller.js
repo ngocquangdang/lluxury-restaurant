deb95633f4af033973b8dffb8852cf07f34afd34
Quang98 <ngocquang.nq98@gmail.com>
2019-05-23 00:05:38 +0700

    mixin pug and add menu-grid

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,4 +1,7 @@
 module.exports.index= (req, res)=>{
     res.render('index')
 }
+module.exports.menu= (req, res)=>{
+    res.render('./menu-page/menu-grid')
+}
 
--- routers/index.router.js
+++ routers/index.router.js
@@ -4,5 +4,6 @@ const index = require('../controllers/index.controller')
 var router = express.Router()
 
 router.get('/', index.index)
+router.get('/menu', index.menu)
 
 module.exports = router
